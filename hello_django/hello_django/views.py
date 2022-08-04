9c670ef91be79b1397d74ee0bdf1c7a455c059a1
Andrey <tandruhas@gmail.com>
2022-08-04 19:41:56 +0300

    Initial commit

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    a = 5 + 6
+    return render(request,'about.html',{'gretting':a})
+
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
+
+def home(request):
+    return HttpResponse('This is my home')
